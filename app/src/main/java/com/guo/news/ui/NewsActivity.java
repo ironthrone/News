20e5e542c6141dfe39d5e93fc4017ff5cd264a54
laozhuangg <laozhuangg@gmail.com>
2016-12-05 21:34:20 +0800

    failed content transition from detail activity

--- app/src/main/java/com/guo/news/ui/NewsActivity.java
+++ app/src/main/java/com/guo/news/ui/NewsActivity.java
@@ -3,11 +3,9 @@ package com.guo.news.ui;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
-import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -33,7 +31,6 @@ import com.guo.news.ui.widget.AsyncImageGetter;
 import com.guo.news.util.Utility;
 import com.squareup.picasso.Picasso;
 
-import java.io.InputStream;
 import java.util.List;
 
 import butterknife.Bind;
