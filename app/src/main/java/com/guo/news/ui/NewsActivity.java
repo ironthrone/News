b4c692100c17f59f4f14451b79b8a350e1caf1a7
ironthrone <laozhuangg@gmail.com>
2016-12-07 17:11:53 +0800

    comment leave transition

--- app/src/main/java/com/guo/news/ui/NewsActivity.java
+++ app/src/main/java/com/guo/news/ui/NewsActivity.java
@@ -97,19 +97,20 @@ public class NewsActivity extends AppCompatActivity implements LoaderManager.Loa
             }
         };
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            getWindow().getEnterTransition().addListener(new AbstractTransitionListener() {
-                @Override
-                public void onTransitionStart(Transition transition) {
-                    if (!mRevealed) {
 
+                getWindow().getDecorView().addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
+                    @Override
+                    public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
+                        v.removeOnLayoutChangeListener(this);
                         int centerX = app_bar.getWidth() / 2;
                         int centerY = app_bar.getHeight() / 2;
                         double endRadius = Math.hypot(centerX, centerY);
                         ViewAnimationUtils.createCircularReveal(app_bar, centerX, centerY, 0, ((float) endRadius)).start();
+
                     }
-                }
-            });
+                });
         }
+
 
         loadData();
     }
